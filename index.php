fa2ea0dd8e5d30b4b8f23c3495369d3b6a733072
Olivier02500 <olivier.peuchet@hotmail.com>
2022-02-23 21:47:17 +0100

    make exo 195

--- index.php
+++ index.php
@@ -15,46 +15,172 @@
  * Une seule requête est permise pour chaque point de l'exo.
  */
 
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$dbName = 'bdd_cours';
+
+
+
 // TODO Commencez par créer votre objet de connexion à la base de données, vous pouvez aussi utiliser l'objet statique ou autre qu'on a créé ensemble.
+try {
+    $dbb = new PDO("mysql:host=$server;dbname=$dbName;charset=utf8", $user, $pass);
+    $dbb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbb->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-/* 1. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' */
-// TODO votre code ici.
+    echo "<p>" . "1. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor'" . "</p>";
+    // TODO votre code ici.
 
-/* 2. Sélectionnez et affichez tous les utilisateurs dont le prénom est différent de 'John' */
-// TODO Votre code ici.
+    $stmt = $dbb->prepare("SELECT * FROM user WHERE nom='Conor'");
+    if ($stmt->execute()) {
+        foreach ($stmt->fetchAll() as $user) {
+            echo "<div>" . "Utilisateur : " . $user['nom'] . "</div>";
+        }
+    }
+    echo "<hr><br>";
 
-/* 3. Sélectionnez et affichez tous les utilisateurs dont l'id est plus petit ou égal à 2 */
-// TODO Votre code ici.
+    echo "<p>" . "2. Sélectionnez et affichez tous les utilisateurs dont le prénom est différent de 'John'" . "</p>";
+    // TODO Votre code ici.
+    $stmt = $dbb->prepare("SELECT * FROM user WHERE prenom != 'John'");
+    if ($stmt->execute()) {
+        foreach ($stmt->fetchAll() as $fname) {
+            echo "<div>" . "Utilisateur : " . $fname['prenom'] . "</div>";
 
-/* 4. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand ou égal à 2 */
-// TODO Votre code ici.
+        }
+        echo "<hr><br>";
 
-/* 5. Sélectionnez et affichez tous les utilisateurs dont l'id est égal à 1 */
-// TODO Votre code ici.
+        echo "<p>" . "3. Sélectionnez et affichez tous les utilisateurs dont l'id est plus petit ou égal à 2" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE id <= 2");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . $id['id'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
 
-/* 6. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand que 1 ET le nom est égal à 'Doe' */
-// TODO Votre code ici.
+        echo "<p>" . "4. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand ou égal à 2" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE id >= 2");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . $id['id'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
 
-/* 7. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Doe' ET le prénom est 'John'*/
-// TODO Votre code ici.
+        echo "<p>" . "5. Sélectionnez et affichez tous les utilisateurs dont l'id est égal à 1" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE id = 1");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . $id['id'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
 
-/* 8. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' OU le prénom est 'Jane' */
-// TODO Votre code ici.
+        echo "<p>" . "6. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand que 1 ET le nom est égal à 'Doe'" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE id = 1 AND nom='Doe'");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur :" . "<br>" .
+                    "ID :" . $id['id'] . "</br>" .
+                    " name : " . $id['nom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
 
-/* 9. Sélectionnez et affichez tous les utilisateurs en limitant les réultats à 2 résultats */
-// TODO Votre code ici.
+        echo "<p>" . "7. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Doe' ET le prénom est 'John'" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE nom='Doe' AND prenom='John'");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur :" . "<br>" .
+                    "ID :" . $id['id'] . "</br>" .
+                    " firstname : " . $id['prenon'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "8. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' OU le prénom est 'Jane'" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE nom='Conor' OR prenom='Jane'");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
 
-/* 10. Sélectionnez et affichez tous les utilisateurs par ordre croissant, en limitant le résultat à 1 seul enregistrement */
-// TODO Votre code ici.
-
-/* 11. Sélectionnez et affichez tous les utilisateurs dont le nom commence par C, fini par r et contient 5 caractères ( voir LIKE )*/
-// TODO Votre code ici.
-
-/* 12. Sélectionnez et affichez tous les utilisateurs dont le nom contient au moins un 'e' */
-// TODO Votre code ici.
-
-/* 13. Sélectionnez et affichez tous les utilisateurs dont le prénom est ( IN ) (John, Sarah) ... voir IN , pas OR '' */
-// TODO Votre code ici.
-
-/* 14. Sélectionnez et affichez tous les utilisateurs dont l'id est situé entre 2 et 4 */
-// TODO Votre code ici.
+        echo "<p>" . "9. Sélectionnez et affichez tous les utilisateurs en limitant les réultats à 2 résultats" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user LIMIT 2");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "10. Sélectionnez et affichez tous les utilisateurs par ordre croissant, en limitant le résultat à 1 seul enregistrement" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user LIMIT 2");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "11. Sélectionnez et affichez tous les utilisateurs dont le nom commence par C, fini par r et contient 5 caractères ( voir LIKE )" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE nom LIKE 'C___r'");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "12. Sélectionnez et affichez tous les utilisateurs dont le nom contient au moins un 'e'" . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE nom LIKE '%e%'");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "13. Sélectionnez et affichez tous les utilisateurs dont le prénom est ( IN ) (John, Sarah) ... voir IN , pas OR ''" . "<p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE prenom IN ('John', 'Sarah')");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+        echo "<hr><br>";
+        echo "<p>" . "14. Sélectionnez et affichez tous les utilisateurs dont l'id est situé entre 2 et 4." . "</p>";
+        // TODO Votre code ici.
+        $stmt = $dbb->prepare("SELECT * FROM user WHERE id BETWEEN 2 AND 4");
+        if ($stmt->execute()) {
+            foreach ($stmt->fetchAll() as $id) {
+                echo "<div>" . "Utilisateur : " . "<br>" .
+                    "name :" . $id['nom'] . "<br>" .
+                    "Firstname : " . $id['prenom'] . "</div>";
+            }
+        }
+    }
+}
+catch (PDOException $e){
+    echo $e->getMessage() ;
+}
